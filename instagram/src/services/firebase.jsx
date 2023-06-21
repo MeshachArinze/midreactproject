d87bdc2c3853c798c705ff2798c00670d3348aed
MeshachArinze <meshacharinze@gmail.com>
2023-06-21 04:09:31 +0100

    ommit to repo

--- instagram/src/services/firebase.jsx
+++ instagram/src/services/firebase.jsx
@@ -21,6 +21,49 @@ export async function getUserByUsername(username) {
   }));
 }
 
-export async function  getUserByUserID() {
-    
+// get user from the firestore where userId === userId (passed from the auth)
+export async function  getUserByUserID(userId) {
+    const result = await db.collection('users').where('userId', '==', userId).get();
+    const user = result.docs.map((item) => ({
+        ...item.data(),
+        docId: item.id
+    }));
+
+    return user;
+}
+
+// check all conditions before limit results
+
+export async function getSuggestedProfiles(userId, following) {
+    let query = db.collection('users');
+
+    if (following.length > 0) {
+        query = query.where('userId', 'not-in', [...following, userId]);
+    } else {
+        query = query.where('userId', '!=', userId)
+    }
+
+    const result = await query.limit(10).get();
+
+    const profiles = result.docs.map((user) => ({
+        ...user.data(),
+        docId: user.id
+    }));
+
+    return profiles;
+}
+
+export async function updateLoggedInUserFollowing(
+  loggedInUserDocId, // currently logged in user document id (karl's profile)
+  profileId, // the user that karl requests to follow
+  isFollowingProfile // true/false (am i currently following this person?)
+) {
+  return db
+    .collection('users')
+    .doc(loggedInUserDocId)
+    .update({
+      following: isFollowingProfile ?
+        FieldValue.arrayRemove(profileId) :
+        FieldValue.arrayUnion(profileId)
+    });
 }
